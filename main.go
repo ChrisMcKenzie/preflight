8690151109f8afc966ba348a5028557c46939b2e
ChrisMcKenzie <chris@chrismckenzie.io>
2016-09-30 08:24:05 -0700

    show task type

--- main.go
+++ main.go
@@ -30,9 +30,10 @@ func main() {
 	}
 
 	yellow := color.New(color.FgYellow).SprintFunc()
+	green := color.New(color.FgGreen).SprintFunc()
 	for _, task := range cl.Tasks {
 		b, _ := json.MarshalIndent(task.Config, "", "  ")
-		fmt.Printf("===== TASK: %s =====\n\n%s\n\n", task.Name, yellow(string(b)))
+		fmt.Printf("===== TASK: (%s) %s =====\n\n%s\n\n", green(task.Type), task.Name, yellow(string(b)))
 	}
 	// cl.Resolve()
 }
